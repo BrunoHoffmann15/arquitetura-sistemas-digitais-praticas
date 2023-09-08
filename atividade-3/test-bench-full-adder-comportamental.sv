891548a0dad1f37c980ff978b98933c3b1a88503
brunoshoffmann <bruno.siqueira.hoffmann@gmail.com>
2023-09-07 21:29:19 -0300

    feat: alterando testes do full adder de 1 bit

--- atividade-3/test-bench-full-adder-comportamental.sv
+++ atividade-3/test-bench-full-adder-comportamental.sv
@@ -12,21 +12,27 @@ module test_bench_full_adder_behavior();
 
     A = 1; B = 0; CIN = 0; #10;
     
-    $display("Simulation 1: A=%b; B=%b; CIN=%b;", A, B, CIN);
-    $display("COUT=%b", COUT);
-    $display("SUM=%b", SUM);
+    $display("Simulation 1: A=%b; B=%b; CIN=%b; COUT=%b; SUM=%b;", A, B, CIN, COUT, SUM);
+
+    if (COUT != 0 | SUM != 1) $display("Failed on scenario 1 should be SUM=1; COUT=0");
     
     A = 1; B = 1; CIN = 0; #10;
     
-    $display("Simulation 2: A=%b; B=%b; CIN=%b;", A, B, CIN);
-    $display("COUT=%b", COUT);
-    $display("SUM=%b", SUM);
+    $display("Simulation 2: A=%b; B=%b; CIN=%b; COUT=%b; SUM=%b;", A, B, CIN, COUT, SUM);
+
+    if (COUT != 1 | SUM != 0) $display("Failed on scenario 1 should be SUM=0; COUT=1");
     
     A = 1; B = 1; CIN = 1; #10;
     
-    $display("Simulation 3: A=%b; B=%b; CIN=%b;", A, B, CIN);
-    $display("COUT=%b", COUT);
-    $display("SUM=%b", SUM);
+    $display("Simulation 3: A=%b; B=%b; CIN=%b; COUT=%b; SUM=%b;", A, B, CIN, COUT, SUM);
+
+    if (COUT != 1 | SUM != 1) $display("Failed on scenario 1 should be SUM=1; COUT=1");
+
+    A = 0; B = 1; CIN = 0; #10;
+    
+    $display("Simulation 4: A=%b; B=%b; CIN=%b; COUT=%b; SUM=%b;", A, B, CIN, COUT, SUM);
+
+    if (COUT != 0 | SUM != 1) $display("Failed on scenario 1 should be SUM=1; COUT=0");
     
     $display("End tests Full Adder:");
   end
@@ -39,7 +45,7 @@ module test_bench_full_adder_8_bits_comportamental();
   full_adder_8_bits_behavior fa8(CIN, A1, B1, A2, B2, A3, B3, A4, B4, A5, B5, A6, B6, A7, B7, A8, B8, S1, S2, S3, S4, S5, S6, S7, S8, COUT);
   
   initial begin
-    #40
+    #50
     $dumpfile("full_adder_8_bits.vcd");
 	  $dumpvars(1);
     
